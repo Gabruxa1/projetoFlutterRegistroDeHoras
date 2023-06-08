848545f59fc7efdb783b583c71f776fa8e3a8f1b
Gabriel <gabriel.harik@gmail.com>
2023-06-08 17:47:11 -0300

    horizonte de implementações

--- ponto_horas/lib/main.dart
+++ ponto_horas/lib/main.dart
@@ -14,7 +14,7 @@ class RegistroPontoApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       supportedLocales: [
-        const Locale('pt', 'BR'), // Adicione outras localizações, se necessário
+        const Locale('en', 'US'), // Adicione outras localizações, se necessário
       ],
       home: RegistroPontoScreen(),
     );
@@ -28,60 +28,63 @@ class RegistroPontoScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Directionality(
-      textDirection: ltr, // Defina a direção desejada
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Registro de Ponto'),
-        ),
-        body: Container(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            children: [
-              TextField(
-                controller: entradaController,
-                decoration: InputDecoration(
-                  labelText: 'Hora de Entrada',
-                ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Registro de Ponto'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: entradaController,
+              decoration: InputDecoration(
+                labelText: 'Hora de Entrada',
               ),
-              TextField(
-                controller: saidaController,
-                decoration: InputDecoration(
-                  labelText: 'Hora de Saída',
-                ),
+            ),
+            TextField(
+              controller: saidaController,
+              decoration: InputDecoration(
+                labelText: 'Hora de Saída',
               ),
-              TextField(
-                controller: almocoController,
-                decoration: InputDecoration(
-                  labelText: 'Horas de Almoço',
-                ),
+            ),
+            TextField(
+              controller: almocoController,
+              decoration: InputDecoration(
+                labelText: 'Horas de Almoço',
               ),
-              ElevatedButton(
-                onPressed: () {
-                  // Lógica para registrar as horas trabalhadas
-                },
-                child: Text('Registrar'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(2021),
-                    lastDate: DateTime(2025),
-                    locale: Locale('pt', 'BR'), // Defina a localização desejada
-                  ).then((selectedDate) {
-                    if (selectedDate != null) {
-                      String formattedDate =
-                          DateFormat.yMMMMd('pt_BR').format(selectedDate);
-                        //Use formattedDate como necessário
-                    }
-                  });
-                },
-                child: Text('Selecionar Data'),
-              ),
-            ],
-          ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Lógica para registrar as horas trabalhadas
+                String entrada = entradaController.text;
+                String saida = saidaController.text;
+                String almoco = almocoController.text;
+
+                // Fazer os cálculos e a lógica necessária para registrar as horas trabalhadas
+              },
+              child: Text('Registrar'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2021),
+                  lastDate: DateTime(2025),
+                  locale: Locale('pt', 'BR'), // Defina a localização desejada como 'pt_BR'
+                ).then((selectedDate) {
+                  if (selectedDate != null) {
+                    String formattedDate =
+                        DateFormat.yMMMMd('pt_BR').format(selectedDate);
+                    // Use formattedDate conforme necessário
+                    print(formattedDate);
+                  }
+                });
+              },
+              child: Text('Selecionar Data'),
+            ),
+          ],
         ),
       ),
     );
